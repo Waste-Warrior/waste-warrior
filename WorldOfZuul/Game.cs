b72e451a222263fff613512360cdcc9b5387f103
KIO-LAPTOP\oskar <oskarvengengel@gmail.com>
2023-10-12 21:27:50 +0200

    Added a few comments

--- WorldOfZuul/Game.cs
+++ WorldOfZuul/Game.cs
@@ -12,7 +12,9 @@
 
         private void CreateRooms()
         {
+            //The trash arrays have to be set outside of the room declaration
             Trash[] outsideTrash = {new ("empty beer can", Trash.TrashType.Metal)};
+            
             Room outside = new("Outside", "You are standing outside the main entrance of the university. To the east is a large building, to the south is a computing lab, and to the west is the campus pub.", outsideTrash);
             Room theatre = new("Theatre", "You find yourself inside a large lecture theatre. Rows of seats ascend up to the back, and there's a podium at the front. It's quite dark and quiet.");
             Room pub = new("Pub", "You've entered the campus pub. It's a cozy place, with a few students chatting over drinks. There's a bar near you and some pool tables at the far end.");
@@ -103,7 +105,7 @@
                         {
                             Trash? collectedTrash = currentRoom?.RemoveTrash(command.RemainingInput);
 
-                            if (collectedTrash != null)
+                            if (collectedTrash != null) //This check may not be needed
                             {
                                 Console.WriteLine($"You collected {collectedTrash.Name}");
                             }
