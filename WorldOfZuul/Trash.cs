5d39bca028572820b98f24ee78f6d9f79860255f
svenons <gotvonisvens@gmail.com>
2023-10-30 18:48:10 +0100

    added categories

--- WorldOfZuul/Trash.cs
+++ WorldOfZuul/Trash.cs
@@ -1,10 +1,28 @@
-﻿namespace WorldOfZuul;
+﻿using System.Runtime.Serialization;
+namespace WorldOfZuul;
 
 public class Trash
 {
-    public enum TrashType
+    public enum TrashType 
     {
-        Metal
+        [EnumMember(Value = "Metal")]
+        Metal = 0,
+        [EnumMember(Value = "Glas")]
+        Glas = 1,
+        [EnumMember(Value = "Papir & pap")]
+        PapirOgPap = 2,
+        [EnumMember(Value = "Madaffald")]
+        Madaffald = 3,
+        [EnumMember(Value = "Plast")]
+        Plast = 4,
+        [EnumMember(Value = "Plast + MAD-& Drikkekartoner")]
+        DrikkeKartoner = 5,
+        [EnumMember(Value = "Restaffald")]
+        Restaffald = 6,
+        [EnumMember(Value = "Batterier")]
+        Batterier = 7,
+        [EnumMember(Value = "Miljøkasse")]
+        Miljøkasse = 8
     }
     
     public readonly string Name;
