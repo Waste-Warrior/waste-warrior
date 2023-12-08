e4f682ad12f3a4a82b09b13e413037ecf8b8bdfb
Oskar Veng Engel <oskarvengengel@gmail.com>
2023-12-08 10:23:12 +0100

    actually working commands

--- WorldOfZuul/Parser.cs
+++ WorldOfZuul/Parser.cs
@@ -2,14 +2,11 @@
 {
     public class Parser
     {
-        private readonly CommandWords commandWords = new();
-
         public Command? GetCommand(string inputLine)
         {
-            
             string[] words = inputLine.ToLower().Split(); //The parser will convert every command to lowercase and put it into an array
 
-            if (words.Length == 0 || !commandWords.IsValidCommand(words[0]))
+            if (words.Length == 0)
             {
                 return null;
             }
@@ -28,7 +25,7 @@
                 }
                 return new Command(words[0], remainingCommand);
             }
-
+            
             return new Command(words[0]);
         }
     }
