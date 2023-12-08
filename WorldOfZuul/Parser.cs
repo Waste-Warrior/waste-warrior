3e5e56a85e2285b8fc5342464a7002067a6788d4
Oskar Veng Engel <oskarvengengel@gmail.com>
2023-12-08 22:21:42 +0100

    reduced parser

--- WorldOfZuul/Parser.cs
+++ WorldOfZuul/Parser.cs
@@ -4,26 +4,11 @@
     {
         public Command? GetCommand(string inputLine)
         {
-            string[] words = inputLine.ToLower().Split(); //The parser will convert every command to lowercase and put it into an array
+            string[] words = inputLine.ToLower().Split(" ", 1); //The parser will convert every command to lowercase and put it into an array
 
             if (words.Length == 0)
             {
                 return null;
-            }
-
-            if (words.Length > 1)
-            {
-                //Maybe the remaining words from the input should be collected in a separate array instead of a string, this could be useful if a command uses multiple arguments.
-                string? remainingCommand = null; 
-                for (int i = 1; i < words.Length; i++)
-                {
-                    remainingCommand += words[i];
-                    if (i != words.Length - 1)
-                    {
-                        remainingCommand += " ";
-                    }
-                }
-                return new Command(words[0], remainingCommand);
             }
             
             return new Command(words[0]);
